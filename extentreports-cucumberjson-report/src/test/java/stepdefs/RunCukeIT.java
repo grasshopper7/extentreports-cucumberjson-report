e5046185544606ae417178029a3975f85e39279e
grasshopper7 <grass.hopper.moc@gmail.com>
2020-09-09 17:16:52 +0530

    Updated for 2.0.0

--- extentreports-cucumberjson-report/src/test/java/stepdefs/RunCukeIT.java
+++ extentreports-cucumberjson-report/src/test/java/stepdefs/RunCukeIT.java
@@ -6,7 +6,8 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;*/
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = { "json:target/cucumber-json1.json", "summary" } /* , tags = "@both" */   )
+@CucumberOptions(plugin = { /* "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", */
+		"json:target/cucumber-json.json", "summary" } /* , tags = "@both" */ )
 public class RunCukeIT extends AbstractTestNGCucumberTests {
 
 }
