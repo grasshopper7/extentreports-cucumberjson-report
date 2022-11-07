daa511fd8851429bedd5a68c0bf46079610fd109
grasshopper7 <grass.hopper.moc@gmail.com>
2022-11-07 22:04:35 +0530

    update version

--- extentreports-cucumberjson-report/src/test/java/stepdefs/RunCukeIT.java
+++ extentreports-cucumberjson-report/src/test/java/stepdefs/RunCukeIT.java
@@ -5,8 +5,8 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/stepdefs/",plugin = { "json:target/cucumber-json.json",
-		"summary" }/* , tags = "@exception or @failure or @skip" */)
+@CucumberOptions(features = "src/test/resources/stepdefs/", plugin = { "json:target/cucumber-json.json",
+		"summary" } /* , tags = "@both" */)
 public class RunCukeIT extends AbstractTestNGCucumberTests {
 
 	/*
